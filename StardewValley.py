fe7bfe76e9cf9420419d895591448ac78f5858e9
kawan74 <kawanfreitas05@gmail.com>
2025-01-08 10:31:37 -0300

    update

--- StardewValley.py
+++ StardewValley.py
@@ -1,82 +1,125 @@
 import glfw
 from OpenGL.GL import *
 
-def inicio():
-    glClearColor(1,1,1,1)
+def setup_background():
+    glClearColor(0.5, 0.8, 1.0, 1.0) #ceu
 
-def cerca():
-    verticesCercaPe1 = [
-        [-0.1,-0.1],
-        [-0.1,0.3],
-        [0.0,0.3],
-        [0.0,-0.1]
+def draw_fence():
+    glColor3f(0.5, 0.3, 0.1)  #vertical
+    for i in range(-5, 6):
+        x = i * 0.1
+        vertices = [
+            [x - 0.02, -0.2],
+            [x - 0.02, 0.1],
+            [x + 0.02, 0.1],
+            [x + 0.02, -0.2]
+        ]
+        glBegin(GL_QUADS)
+        for vertex in vertices:
+            glVertex2fv(vertex)
+        glEnd()
+
+    glColor3f(0.6, 0.4, 0.2)  #horizontal
+    for y_offset in [-0.05, 0.05]:
+        vertices = [
+            [-0.6, y_offset - 0.01],
+            [-0.6, y_offset + 0.01],
+            [0.6, y_offset + 0.01],
+            [0.6, y_offset - 0.01]
+        ]
+        glBegin(GL_QUADS)
+        for vertex in vertices:
+            glVertex2fv(vertex)
+        glEnd()
+
+def draw_house():
+    glColor3f(0.9, 0.6, 0.3)  #casa
+    body_vertices = [
+        [-0.3, -0.2],
+        [-0.3, 0.2],
+        [0.3, 0.2],
+        [0.3, -0.2]
     ]
-
-    glColor3f(128/255, 59/255, 18/255)
     glBegin(GL_QUADS)
-    for v in verticesCercaPe1:
-        glVertex2fv(v)
+    for vertex in body_vertices:
+        glVertex2fv(vertex)
     glEnd()
 
-    verticesCercaDeitado1 = [
-        [-0.1, 0.125],
-        [-0.1, 0.2],
-        [0.15, 0.2],
-        [0.15, 0.125]
+    #telhado
+    glColor3f(0.7, 0.1, 0.1) 
+    roof_vertices = [
+        [-0.4, 0.2],
+        [0.4, 0.2],
+        [0.0, 0.5]
     ]
-
-    glPushMatrix()
-    glColor3f(128/255, 59/255, 18/255)
-    glBegin(GL_QUADS)
-    for v in verticesCercaDeitado1:
-        glVertex2fv(v)
+    glBegin(GL_TRIANGLES)
+    for vertex in roof_vertices:
+        glVertex2fv(vertex)
     glEnd()
-    glPopMatrix()
 
-    verticesCercaDeitado2 = [
-        [-0.1, 0.0],
-        [-0.1, 0.075],
-        [0.15, 0.075],
-        [0.15, 0.0]
+    #porta
+    glColor3f(0.5, 0.3, 0.1)  
+    door_vertices = [
+        [-0.05, -0.2],
+        [-0.05, 0.05],
+        [0.05, 0.05],
+        [0.05, -0.2]
     ]
-    
-    glPushMatrix()
-    glColor3f(128/255, 59/255, 18/255)
     glBegin(GL_QUADS)
-    for v in verticesCercaDeitado2:
-        glVertex2fv(v)
+    for vertex in door_vertices:
+        glVertex2fv(vertex)
     glEnd()
-    glPopMatrix()
 
-    verticesCercaPe2 = [
-        [0.15,-0.1],
-        [0.15,0.3],
-        [0.25,0.3],
-        [0.25,-0.1]
+    #janela
+    glColor3f(0.7, 0.9, 1.0)  
+    window_vertices = [
+        [[-0.25, 0.0], [-0.15, 0.0], [-0.15, 0.1], [-0.25, 0.1]],
+        [[0.15, 0.0], [0.25, 0.0], [0.25, 0.1], [0.15, 0.1]]
     ]
+    for vertices in window_vertices:
+        glBegin(GL_QUADS)
+        for vertex in vertices:
+            glVertex2fv(vertex)
+        glEnd()
 
-    glPushMatrix()
-    glColor3f(128/255, 59/255, 18/255)
+def draw_grass():
+    glColor3f(0.3, 0.7, 0.2)  #grama
+    grass_vertices = [
+        [-1.0, -0.5],
+        [-1.0, -0.2],
+        [1.0, -0.2],
+        [1.0, -0.5]
+    ]
     glBegin(GL_QUADS)
-    for v in verticesCercaPe2:
-        glVertex2fv(v)
+    for vertex in grass_vertices:
+        glVertex2fv(vertex)
     glEnd()
-    glPopMatrix()
 
-def render():
+def render_scene():
     glClear(GL_COLOR_BUFFER_BIT)
-    cerca()
+    draw_grass()
+    draw_house()
+    draw_fence()
 
 def main():
-    glfw.init()                                                      
-    window = glfw.create_window(500,500,'Stardew Valley',None,None)
-    glfw.make_context_current(window)                               
-    inicio()                                                    
-    while not glfw.window_should_close(window):                     
-        glfw.poll_events()                                          
-        render()                                                    
-        glfw.swap_buffers(window)                                   
-    glfw.terminate()                                                
+    if not glfw.init():
+        return
 
-if __name__ == '__main__':
+    window = glfw.create_window(500, 500, "Stardew Valley", None, None)
+    if not window:
+        glfw.terminate()
+        return
+
+    glfw.make_context_current(window)
+    setup_background()
+
+    while not glfw.window_should_close(window):
+        glfw.poll_events()
+        render_scene()
+        glfw.swap_buffers(window)
+
+    glfw.terminate()
+
+if __name__ == "__main__":
     main()
+    
